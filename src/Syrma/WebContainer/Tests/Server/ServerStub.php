e7a9d67d1f66d201d7a7a4f213a14620e41aaf35
Szijarto Tamas <szijarto.tamas.developer@gmail.com>
2015-07-23 01:25:26 +0200

    add ServerStub

---
+++ src/Syrma/WebContainer/Tests/Server/ServerStub.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace Syrma\WebContainer\Tests\Server;
+
+
+use Syrma\WebContainer\RequestHandlerInterface;
+use Syrma\WebContainer\ServerContextInterface;
+use Syrma\WebContainer\ServerInterface;
+
+/**
+ * Server for tests
+ */
+class ServerStub implements ServerInterface{
+
+    /**
+     * @var callable
+     */
+    private $startFn;
+
+    /**
+     * @param callable $startFn
+     */
+    public function __construct(callable $startFn)
+    {
+        $this->startFn = $startFn;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function start(ServerContextInterface $context, RequestHandlerInterface $requestHandler)
+    {
+        return call_user_func($this->startFn, $context, $requestHandler);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function stop()
+    {
+       //nothing
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function isAvaiable()
+    {
+        return true;
+    }
+
+}
