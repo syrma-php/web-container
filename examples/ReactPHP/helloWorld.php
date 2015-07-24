9ba290169c5785623b08d8e6511639b9ef9f9894
Szijarto Tamas <szijarto.tamas.developer@gmail.com>
2015-07-25 00:46:39 +0200

    refact the examples

--- examples/ReactPHP/helloWorld.php
+++ examples/ReactPHP/helloWorld.php
@@ -6,24 +6,30 @@ use Syrma\WebContainer\Server\React\ReactMessageTransformer;
 
 require dirname(dirname(__DIR__)).'/vendor/autoload.php';
 
-    $logger = new \Monolog\Logger('app',array( new StreamHandler(fopen('php://stdout', 'w'))));
-
     $transformer = new ReactMessageTransformer(
         new \Syrma\WebContainer\Util\ZendPsr7Factory()
     );
 
-    $executor = new \Syrma\WebContainer\Executor(
-        new \Syrma\WebContainer\Server\React\ReactServer($transformer),
-        new \Syrma\WebContainer\RequestHandler\CallbackRequestHandler(function (\Psr\Http\Message\RequestInterface $request) {
-
-            $stream = new \Zend\Diactoros\Stream('php://temp', 'wb+');
-            $stream->write('Hello World! - ' . posix_getpid() . PHP_EOL);
-            $response = new \Zend\Diactoros\Response($stream, 200, ['X-Debug-token' => md5(1)]);
-
-            return $response;
-
-        }),
-        $logger
+    $exceptionHandler = new \Syrma\WebContainer\Exception\DefaultExceptionHandler(
+        new \Syrma\WebContainer\Util\ZendPsr7Factory(),
+        \Syrma\WebContainer\Util\ErrorPageLoader::createDefault()
     );
 
+    $requestHandler = function (\Psr\Http\Message\RequestInterface $request) {
+
+        $stream = new \Zend\Diactoros\Stream('php://temp', 'r+');
+        $stream->write('Hello World! - ' . posix_getpid() . PHP_EOL);
+        $response = new \Zend\Diactoros\Response($stream, 200, ['X-Debug-token' => md5(1)]);
+
+        return $response;
+
+    };
+
+    $executor = new \Syrma\WebContainer\Executor(
+        new \Syrma\WebContainer\Server\React\ReactServer($transformer),
+        new \Syrma\WebContainer\RequestHandler\CallbackRequestHandler($requestHandler),
+        $exceptionHandler
+    );
+
+    $executor->setLogger( new \Monolog\Logger('app',array( new StreamHandler(fopen('php://stdout', 'w')))) );
     $executor->execute(new \Syrma\WebContainer\ServerContext());
--- examples/Swoole/helloWorld.php
+++ examples/Swoole/helloWorld.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require dirname(dirname(__DIR__)).'/vendor/autoload.php';
 
     $transformer = new \Syrma\WebContainer\Server\Swoole\SwooleMessageTransformer(
@@ -12,14 +11,24 @@ require dirname(dirname(__DIR__)).'/vendor/autoload.php';
         'worker_num' => 1,
     ));
 
-    $executor = new \Syrma\WebContainer\Executor(
-        new \Syrma\WebContainer\Server\Swoole\SwooleServer($transformer, $options),
-        new \Syrma\WebContainer\RequestHandler\CallbackRequestHandler(function (\Psr\Http\Message\RequestInterface $request) {
-
-            $stream = new \Zend\Diactoros\Stream('php://temp', 'wb+');
-            $stream->write('Hello World!');
-            return new \Zend\Diactoros\Response($stream, 200, ['X-Debug-token' => md5(1)]);
-        })
+    $exceptionHandler = new \Syrma\WebContainer\Exception\DefaultExceptionHandler(
+        new \Syrma\WebContainer\Util\ZendPsr7Factory(),
+        \Syrma\WebContainer\Util\ErrorPageLoader::createDefault()
     );
 
+    $requestHandler = function (\Psr\Http\Message\RequestInterface $request) {
+
+        $stream = new \Zend\Diactoros\Stream('php://temp', 'r+');
+        $stream->write('Hello World! - ' . posix_getpid() . PHP_EOL);
+        $response = new \Zend\Diactoros\Response($stream, 200, ['X-Debug-token' => md5(1)]);
+
+        return $response;
+    };
+
+    $executor = new \Syrma\WebContainer\Executor(
+        new \Syrma\WebContainer\Server\Swoole\SwooleServer($transformer, $options),
+        new \Syrma\WebContainer\RequestHandler\CallbackRequestHandler($requestHandler),
+        $exceptionHandler
+    );
+    $executor->setLogger( new \Monolog\Logger('app',array( new \Monolog\Handler\StreamHandler(fopen('php://stdout', 'w')))) );
     $executor->execute(new \Syrma\WebContainer\ServerContext());
