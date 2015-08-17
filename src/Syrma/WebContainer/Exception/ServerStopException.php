49f1c867d1060ffa73e414dc39414aa49f799587
Szijarto Tamas <szijarto.tamas.developer@gmail.com>
2015-08-17 23:53:14 +0200

    add ServerStopException

---
+++ src/Syrma/WebContainer/Exception/ServerStopException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Syrma\WebContainer\Exception;
+
+/**
+ * Default implementqation of ServerStopException.
+ */
+class ServerStopException extends \RuntimeException implements ServerStopExceptionInterface
+{
+}
